eab3526bf2c3647a8f84494660e82ce3155cd76a
Yobuligo <hoffmannpeter82@gmail.com>
2024-05-21 16:53:27 +0200

    Introduce decorator Provider

--- backend/src/decorators/Provider.ts
+++ backend/src/decorators/Provider.ts
@@ -1,8 +1,11 @@
 import { ProviderConstructor } from "../providers/core/ProviderConstructor";
+import { DecoratorInfo } from "../services/decoratorInfo/DecoratorInfo";
 import { ProviderType } from "../shared/types/ProviderType";
 
 export const Provider = <T extends ProviderConstructor>(
   providerType: ProviderType
 ) => {
-  return (target: T) => {};
+  return (target: T) => {
+    DecoratorInfo.introduce(target, Provider, providerType);
+  };
 };
