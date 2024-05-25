9667bf67abd41ab859cdba79dd7bafa7e5af6a11
Yobuligo <hoffmannpeter82@gmail.com>
2024-05-25 18:51:51 +0200

    Add message if no suitable projects were found

--- frontend/src/features/projectList/ProjectList.tsx
+++ frontend/src/features/projectList/ProjectList.tsx
@@ -1,10 +1,19 @@
+import { texts } from "../../hooks/useTranslation/texts";
+import { useTranslation } from "../../hooks/useTranslation/useTranslation";
 import { Project } from "../project/Project";
 import styles from "./ProjectList.module.scss";
 import { IProjectListProps } from "./ProjectListProps";
 
 export const ProjectList: React.FC<IProjectListProps> = (props) => {
+  const { t } = useTranslation();
+
   const items = props.projects.map((project) => (
     <Project key={project.id} project={project} />
   ));
-  return <div className={styles.projectList}>{items}</div>;
+
+  return (
+    <div className={styles.projectList}>
+      {items.length === 0 ? <>{t(texts.projectList.noProjects)}</> : items}
+    </div>
+  );
 };
