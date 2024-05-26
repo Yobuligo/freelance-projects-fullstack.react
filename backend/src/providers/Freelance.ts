248bb5cb642cf5527e4840b2de434c6b874c0063
Yobuligo <hoffmannpeter82@gmail.com>
2024-05-26 16:12:20 +0200

    Add projects from Freelance

--- backend/src/providers/Freelance.ts
+++ backend/src/providers/Freelance.ts
@@ -3,6 +3,9 @@ import { IProvider } from "./core/IProvider";
 
 export class Freelance implements IProvider {
   request(url: string): Promise<IProject[]> {
-    return new Promise(async (resolve, reject) => {});
+    return new Promise(async (resolve, reject) => {
+      const projects: IProject[] = [];
+      resolve(projects);
+    });
   }
 }
