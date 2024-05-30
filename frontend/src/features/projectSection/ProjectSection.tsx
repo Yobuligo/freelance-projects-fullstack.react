abccb1cd2291933011741aececbb8cd911aea131
Yobuligo <hoffmannpeter82@gmail.com>
2024-05-30 23:05:41 +0200

    Introduce useProjectSectionViewModel

--- frontend/src/features/projectSection/ProjectSection.tsx
+++ frontend/src/features/projectSection/ProjectSection.tsx
@@ -1,69 +1,25 @@
-import { useState } from "react";
-import { ProjectApi } from "../../api/ProjectApi";
 import { ReactComponent as SettingsIcon } from "../../assets/settings.svg";
 import { Button } from "../../components/button/Button";
 import { useInitialize } from "../../hooks/useInitialize";
-import { useProjectIdStorage } from "../../hooks/useProjectIdStorage";
 import { texts } from "../../hooks/useTranslation/texts";
 import { useTranslation } from "../../hooks/useTranslation/useTranslation";
-import { IProject } from "../../shared/model/IProject";
 import { CompletedSection } from "../completedSection/CompletedSection";
 import { ProjectList } from "../projectList/ProjectList";
-import { IProjectSectionProps } from "./IProjectSectionProps";
 import styles from "./ProjectSection.module.scss";
+import { useProjectSectionViewModel } from "./useProjectSectionViewModel";
 
-export const ProjectSection: React.FC<IProjectSectionProps> = (props) => {
+export const ProjectSection: React.FC = () => {
   const { t } = useTranslation();
-  const [isLoading, setIsLoading] = useState(false);
-  const [projects, setProjects] = useState<IProject[]>([]);
-  const projectIdStorage = useProjectIdStorage();
+  const viewModel = useProjectSectionViewModel();
 
-  const loadProjects = async () => {
-    setIsLoading(true);
-    const projects = await ProjectApi.findAll();
-    projects.forEach((project) => {
-      const index = projectIdStorage.checkedProjectIds.findIndex(
-        (projectId) => projectId === project.id
-      );
-      if (index !== -1) {
-        project.completed = true;
-      }
-    });
-
-    setProjects(projects);
-    setIsLoading(false);
-  };
-
-  useInitialize(loadProjects);
-
-  const openProjects = projects.filter((project) => !project.completed);
-
-  const completedProjects = projects.filter((project) => project.completed);
-
-  const onProjectChecked = (project: IProject) => {
-    setProjects((previous) => {
-      project.completed = true;
-      return [...previous];
-    });
-    projectIdStorage.setChecked(project);
-  };
-
-  const onProjectUnchecked = (project: IProject) => {
-    setProjects((previous) => {
-      project.completed = false;
-      return [...previous];
-    });
-    projectIdStorage.setUnchecked(project);
-  };
-
-  const onReload = () => loadProjects();
+  useInitialize(viewModel.loadProjects);
 
   return (
     <div className={styles.projectSection}>
       <div className={styles.reloadButton}>
         <Button
           caption={t(texts.projectSection.captionReloadButton)}
-          onClick={onReload}
+          onClick={viewModel.onReload}
         />
         <Button>
           <SettingsIcon />
@@ -72,16 +28,16 @@ export const ProjectSection: React.FC<IProjectSectionProps> = (props) => {
 
       <>
         <ProjectList
-          isLoading={isLoading}
-          onChecked={onProjectChecked}
-          onUnchecked={onProjectUnchecked}
-          projects={openProjects}
+          isLoading={viewModel.isLoading}
+          onChecked={viewModel.onProjectChecked}
+          onUnchecked={viewModel.onProjectUnchecked}
+          projects={viewModel.openProjects}
         />
         <div className={styles.completedSection}>
           <CompletedSection
-            onChecked={onProjectChecked}
-            onUnchecked={onProjectUnchecked}
-            projects={completedProjects}
+            onChecked={viewModel.onProjectChecked}
+            onUnchecked={viewModel.onProjectUnchecked}
+            projects={viewModel.completedProjects}
           />
         </div>
       </>
---
+++ frontend/src/features/projectSection/useProjectSectionViewModel.ts
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useProjectIdStorage } from "../../hooks/useProjectIdStorage";
+import { IProject } from "../../shared/model/IProject";
+import { ProjectApi } from "../../api/ProjectApi";
+
+export const useProjectSectionViewModel = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [projects, setProjects] = useState<IProject[]>([]);
+  const projectIdStorage = useProjectIdStorage();
+
+  const openProjects = projects.filter((project) => !project.completed);
+  const completedProjects = projects.filter((project) => project.completed);
+
+  const loadProjects = async () => {
+    setIsLoading(true);
+    const projects = await ProjectApi.findAll();
+    projects.forEach((project) => {
+      const index = projectIdStorage.checkedProjectIds.findIndex(
+        (projectId) => projectId === project.id
+      );
+      if (index !== -1) {
+        project.completed = true;
+      }
+    });
+
+    setProjects(projects);
+    setIsLoading(false);
+  };
+
+  const onProjectChecked = (project: IProject) => {
+    setProjects((previous) => {
+      project.completed = true;
+      return [...previous];
+    });
+    projectIdStorage.setChecked(project);
+  };
+
+  const onProjectUnchecked = (project: IProject) => {
+    setProjects((previous) => {
+      project.completed = false;
+      return [...previous];
+    });
+    projectIdStorage.setUnchecked(project);
+  };  
+
+  const onReload = () => loadProjects();
+
+  return {
+    completedProjects,
+    isLoading,
+    loadProjects,
+    onProjectChecked,
+    onProjectUnchecked,
+    onReload,
+    openProjects,
+  };
+};
