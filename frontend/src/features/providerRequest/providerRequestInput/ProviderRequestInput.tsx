9ed73775b767da6c0a7ae4ec54f8f8c5afdf9b08
Peter <hoffmannpeter82@gmail.com>
2024-05-31 14:34:06 +0200

    Provide select for selecting ProviderType

--- frontend/src/features/providerRequest/providerRequestInput/ProviderRequestInput.tsx
+++ frontend/src/features/providerRequest/providerRequestInput/ProviderRequestInput.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { Button } from "../../../components/button/Button";
-import { Card } from "../../../components/card/Card";
+import { ISelectOption } from "../../../components/select/ISelectOption";
+import { Select } from "../../../components/select/Select";
 import { texts } from "../../../hooks/useTranslation/texts";
 import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
+import { ProviderType } from "../../../shared/types/ProviderType";
 import styles from "./ProviderRequestInput.module.scss";
 
 export const ProviderRequestInput: React.FC = () => {
+  const [providerType, setProviderType] = useState(ProviderType.Freelance);
   const { t } = useTranslation();
+
   const onChangeForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+  };
+
+  const selectOptions = (): ISelectOption[] => {
+    const selectOptions: ISelectOption[] = [];
+    for (const propName in ProviderType) {
+      const propValue: string = (ProviderType as any)[propName];
+      selectOptions.push({ key: propValue, text: propValue });
+    }
+    return selectOptions;
   };
 
   return (
@@ -16,7 +30,16 @@ export const ProviderRequestInput: React.FC = () => {
         <label htmlFor="providerType">
           {t(texts.providerRequestInput.providerType)}
         </label>
-        <input id="providerType" type="text" />
+        <Select
+          onSelect={(selectOption) => {
+            setProviderType(selectOption.key as ProviderType);
+          }}
+          selected={{
+            key: providerType,
+            text: (ProviderType as any)[providerType],
+          }}
+          options={selectOptions()}
+        />
       </div>
 
       <div className={styles.input}>
