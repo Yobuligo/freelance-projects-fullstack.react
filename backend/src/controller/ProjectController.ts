f7a0a0db6217e759a5d6df39fbb79b62e649cbf5
Yobuligo <hoffmannpeter82@gmail.com>
2024-06-17 10:43:54 +0200

    Adjust ProjectController

--- backend/src/controller/ProjectController.ts
+++ backend/src/controller/ProjectController.ts
@@ -3,7 +3,6 @@ import { ProjectCollector } from "../services/projectCollector/ProjectCollector"
 import { ProjectMeta } from "../shared/model/IProject";
 import { IProviderRequests } from "../shared/model/IProviderRequests";
 import { sortProjects } from "../utils/sortProjects";
-import { IError } from "../shared/model/IError";
 
 export class ProjectController {
   readonly router = Router();
@@ -14,16 +13,11 @@ export class ProjectController {
 
   private findAll() {
     this.router.post(ProjectMeta.path, async (req, res) => {
-      const error: IError = {
-        createdAt: new Date(),
-        message: "Error while fetching projects",
-      };
-      res.status(400).send(error);
-      // const providerRequests: IProviderRequests[] = req.body;
-      // const projectCollector = new ProjectCollector();
-      // const projects = await projectCollector.collect(providerRequests);
-      // const sortedProjects = sortProjects(projects)
-      // res.status(200).send(sortedProjects);
+      const providerRequests: IProviderRequests[] = req.body;
+      const projectCollector = new ProjectCollector();
+      const projects = await projectCollector.collect(providerRequests);
+      const sortedProjects = sortProjects(projects);
+      res.status(200).send(sortedProjects);
     });
   }
 }
