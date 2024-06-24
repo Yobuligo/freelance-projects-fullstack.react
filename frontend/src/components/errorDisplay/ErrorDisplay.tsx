950b5f4f2b02bba8bf954cacb7dd9659437688b8
Yobuligo <hoffmannpeter82@gmail.com>
2024-06-24 16:58:07 +0200

    Close ErrorDisplay

--- frontend/src/components/errorDisplay/ErrorDisplay.tsx
+++ frontend/src/components/errorDisplay/ErrorDisplay.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import { useErrorMessage } from "../../hooks/useErrorMessage";
+import { CloseIcon } from "../../icons/CloseIcon";
 import { Card } from "../card/Card";
 import styles from "./ErrorDisplay.module.scss";
 
 export const ErrorDisplay: React.FC = () => {
-  const [errorMessage] = useErrorMessage();
+  const [errorMessage, setErrorMessage] = useErrorMessage();
+
+  useEffect(() => {
+    setErrorMessage("Hello");
+  }, [setErrorMessage]);
+
+  const onClose = () => setErrorMessage("");
+
   return (
     <>
       {errorMessage && (
-        <Card className={styles.errorDisplay}>{errorMessage}</Card>
+        <Card className={styles.errorDisplay}>
+          {errorMessage}
+          <CloseIcon onClick={onClose} />
+        </Card>
       )}
     </>
   );
