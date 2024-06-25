f245a0039185dba00335998759b7a1e7faca1067
Yobuligo <hoffmannpeter82@gmail.com>
2024-06-25 13:38:58 +0200

    Set Computer Future as first provider

---
+++ backend/src/providers/ComputerFutures.ts
@@ -0,0 +1,13 @@
+import { Provider } from "../decorators/Provider";
+import { IProject } from "../shared/model/IProject";
+import { ProviderType } from "../shared/types/ProviderType";
+import { IProvider } from "./core/IProvider";
+
+@Provider(ProviderType.ComputerFutures, "Computer Futures")
+export class ComputerFutures implements IProvider {
+  request(url: string): Promise<IProject[]> {
+    return new Promise((resolve, reject) => {
+      resolve([]);
+    });
+  }
+}
--- backend/src/providers/ProviderRegistry.ts
+++ backend/src/providers/ProviderRegistry.ts
@@ -1,5 +1,10 @@
+import { ComputerFutures } from "./ComputerFutures";
 import { Freelance } from "./Freelance";
 import { FreelancerMap } from "./FreelancerMap";
 import { ProviderClassType } from "./core/ProviderClassType";
 
-export const ProviderRegistry: ProviderClassType[] = [FreelancerMap, Freelance];
+export const ProviderRegistry: ProviderClassType[] = [
+  ComputerFutures,
+  FreelancerMap,
+  Freelance,
+];
