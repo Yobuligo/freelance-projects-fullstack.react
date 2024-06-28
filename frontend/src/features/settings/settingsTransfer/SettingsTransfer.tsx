40cfc5bb2a505d7fd01c58893a57d1617c0afeca
Peter <hoffmannpeter82@gmail.com>
2024-06-28 16:18:34 +0200

    Add Settings for upload and download

--- frontend/src/features/settings/settingsTransfer/SettingsTransfer.tsx
+++ frontend/src/features/settings/settingsTransfer/SettingsTransfer.tsx
@@ -1,11 +1,20 @@
+import { texts } from "../../../hooks/useTranslation/texts";
+import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
 import { DownloadIcon } from "../../../icons/DownloadIcon";
 import { UploadIcon } from "../../../icons/UploadIcon";
+import { SettingsItem } from "../settingsItem/SettingsItem";
 
 export const SettingsTransfer: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <UploadIcon />
-      <DownloadIcon />
+      <SettingsItem title={t(texts.settingsTransfer.upload)}>
+        <UploadIcon />
+      </SettingsItem>
+      <SettingsItem title={t(texts.settingsTransfer.download)}>
+        <DownloadIcon />
+      </SettingsItem>
     </div>
   );
 };
