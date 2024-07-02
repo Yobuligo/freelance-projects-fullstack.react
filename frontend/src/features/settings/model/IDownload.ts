c4464ebd1bc49865ee0d78e06ebd111b67adfa32
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-02 09:38:53 +0200

    Download settings and user config to clipboard

---
+++ frontend/src/features/settings/model/IDownload.ts
@@ -0,0 +1,7 @@
+import { ISettings } from "../../../model/ISettings";
+import { IUserConfig } from "../../../model/IUserConfig";
+
+export interface IDownload {
+  settings: ISettings;
+  userConfig: IUserConfig;
+}
--- frontend/src/features/settings/settingsTransfer/SettingsTransfer.tsx
+++ frontend/src/features/settings/settingsTransfer/SettingsTransfer.tsx
@@ -1,16 +1,29 @@
+import { useSettings } from "../../../hooks/useSettings";
 import { texts } from "../../../hooks/useTranslation/texts";
 import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
+import { useUserConfig } from "../../../hooks/useUserConfig";
 import { DownloadIcon } from "../../../icons/DownloadIcon";
 import { UploadIcon } from "../../../icons/UploadIcon";
+import { IDownload } from "../model/IDownload";
 import { SettingsItem } from "../settingsItem/SettingsItem";
 
 export const SettingsTransfer: React.FC = () => {
   const { t } = useTranslation();
+  const [userConfig] = useUserConfig();
+  const [settings] = useSettings();
+
+  const onDownload = () => {
+    const download: IDownload = {
+      settings,
+      userConfig,
+    };
+    navigator.clipboard.writeText(JSON.stringify(download));
+  };
 
   return (
     <>
       <SettingsItem title={t(texts.settingsTransfer.downloadConfig)}>
-        <DownloadIcon />
+        <DownloadIcon onClick={onDownload} />
       </SettingsItem>
       <SettingsItem title={t(texts.settingsTransfer.uploadConfig)}>
         <UploadIcon />
