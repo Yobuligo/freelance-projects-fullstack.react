9e152dd64b7f7efb547d31dbbdec105c6c6d70fa
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-10 23:01:14 +0200

    Add documentation

--- frontend/src/types/Value.ts
+++ frontend/src/types/Value.ts
@@ -1,3 +1,6 @@
+/**
+ * This type represents any kind of value which contains of the value itself and a function to update the value
+ */
 export type Value<T> = [
   value: T,
   setValue: (newValue: T | ((previous: T) => T)) => void
