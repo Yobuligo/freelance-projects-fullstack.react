8b2331d92c670b5d2f37689857ede44b8de034e1
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-16 21:27:42 +0200

    Set ProjectPage as PublicPage

--- frontend/src/pages/ProjectPage.tsx
+++ frontend/src/pages/ProjectPage.tsx
@@ -1,15 +1,15 @@
 import { ErrorDisplay } from "../components/errorDisplay/ErrorDisplay";
-import { ProtectedPage } from "../components/pages/protectedPage/ProtectedPage";
+import { PublicPage } from "../components/pages/publicPage/PublicPage";
 import { ProjectSection } from "../features/project/projectSection/ProjectSection";
 import { ProviderDetailsContextProvider } from "../features/providerDetailsContextProvider/ProviderDetailsContextProvider";
 
 export const ProjectPage: React.FC = () => {
   return (
-    <ProtectedPage>
+    <PublicPage>
       <ProviderDetailsContextProvider>
         <ErrorDisplay />
         <ProjectSection />
       </ProviderDetailsContextProvider>
-    </ProtectedPage>
+    </PublicPage>
   );
 };
