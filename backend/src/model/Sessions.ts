4cb6f2262e30f922bae192d4e97de24f1189d5be
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-21 16:54:22 +0200

    Extend model by parameter expiresAt

--- backend/src/model/Sessions.ts
+++ backend/src/model/Sessions.ts
@@ -5,6 +5,7 @@ import { IEntityDetails } from "../shared/types/IEntityDetails";
 
 export const Sessions: ModelStatic<Model<ISession, IEntityDetails<ISession>>> =
   db.define("sessions", {
+    expiresAt: DataTypes.DATE,
     userId: DataTypes.STRING,
     username: DataTypes.STRING,
   });
