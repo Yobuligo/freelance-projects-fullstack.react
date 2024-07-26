0c3f45c2691c6377316adb5c0472e37416247681
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-26 17:37:28 +0200

    Add index to url and fill publishedAt for IProject

--- backend/src/model/Projects.ts
+++ backend/src/model/Projects.ts
@@ -10,13 +10,18 @@ import { IEntityDetails } from "../shared/types/IEntityDetails";
 import { UserProjects } from "./UserProjects";
 
 const projects: ModelStatic<Model<IProject, IEntityDetails<IProject>>> =
-  db.define("projects", {
-    company: DataTypes.STRING,
-    location: DataTypes.STRING,
-    provider: DataTypes.STRING,    
-    title: DataTypes.STRING,
-    url: DataTypes.STRING,
-  });
+  db.define(
+    "projects",
+    {
+      company: DataTypes.STRING,
+      location: DataTypes.STRING,
+      provider: DataTypes.STRING,
+      publishedAt: DataTypes.DATE,
+      title: DataTypes.STRING,
+      url: DataTypes.STRING,
+    },
+    { indexes: [{ unique: true, fields: ["url"] }] }
+  );
 
 export class Projects extends projects {
   declare getUserProjects: HasManyGetAssociationsMixin<UserProjects>;
