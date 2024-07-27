db477ad0d0bff39ab5b88a0f8f74fbd354f1b5c2
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-27 19:18:21 +0200

    Start to remote ProviderRequest

--- frontend/src/model/IProviderRequest.ts
+++
@@ -1,9 +0,0 @@
-import { ProviderType } from "../shared/types/ProviderType";
-
-export interface IProviderRequest {
-  id: string;
-  enabled: boolean;
-  providerType: ProviderType;
-  providerUrl: string;
-  requestTitle: string;
-}
