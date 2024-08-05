359971ca5a95726eeb4de8673242623b39282f00
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-05 13:50:59 +0200

    Add margin between Settings and ErrorDisplay

--- frontend/src/components/errorDisplay/ErrorDisplay.tsx
+++ frontend/src/components/errorDisplay/ErrorDisplay.tsx
@@ -2,20 +2,21 @@ import { useErrorMessage } from "../../hooks/useErrorMessage";
 import { CloseIcon } from "../../icons/CloseIcon";
 import { Card } from "../card/Card";
 import styles from "./ErrorDisplay.module.scss";
+import { IErrorDisplayProps } from "./IErrorDisplayProps";
 
-export const ErrorDisplay: React.FC = () => {
+export const ErrorDisplay: React.FC<IErrorDisplayProps> = (props) => {
   const [errorMessage, setErrorMessage] = useErrorMessage();
 
   const onClose = () => setErrorMessage("");
 
   return (
-    <>
+    <div className={props.className}>
       {errorMessage && (
         <Card className={styles.errorDisplay}>
           {errorMessage}
           <CloseIcon onClick={onClose} />
         </Card>
       )}
-    </>
+    </div>
   );
 };
--- frontend/src/pages/OpportunityPage.tsx
+++ frontend/src/pages/OpportunityPage.tsx
@@ -2,12 +2,13 @@ import { ErrorDisplay } from "../components/errorDisplay/ErrorDisplay";
 import { ProtectedPage } from "../components/pages/protectedPage/ProtectedPage";
 import { OpportunitySection } from "../features/opportunity/opportunitySection/OpportunitySection";
 import { ProviderDetailsContextProvider } from "../features/providerDetailsContextProvider/ProviderDetailsContextProvider";
+import styles from "./OpportunityPage.module.scss";
 
 export const OpportunityPage: React.FC = () => {
   return (
     <ProtectedPage>
       <ProviderDetailsContextProvider>
-        <ErrorDisplay />
+        <ErrorDisplay className={styles.errorDisplay} />
         <OpportunitySection />
       </ProviderDetailsContextProvider>
     </ProtectedPage>
