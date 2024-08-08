21a3a6b6346a55a6ba07fbe07a80fbb40d206f09
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-08 08:50:38 +0200

    Use AppConfig for initializing database

--- backend/src/AppConfig.ts
+++ backend/src/AppConfig.ts
@@ -1,13 +1,19 @@
-import { Dialect } from "sequelize";
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 export const AppConfig = {
   reloadIntervalMins: 20,
-  DB_DIALECT: process.env.DB_DIALECT as Dialect,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: parseInt(process.env.DB_PORT!),
-  DB_NAME: process.env.DB_NAME,
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbPort: parseInt(process.env.DB_PORT!),
+  dbName: process.env.DB_NAME,
+  dbUsername: process.env.DB_USERNAME,
+  dbPassword: process.env.DB_PASSWORD,
   PEPPER: "c894eed9031e71867d76ca1784fadb0a87ca7cd4b83e2c5603a818d2616576f8",
-  SERVER_PORT: parseInt(process.env.SERVER_PORT!),
 };
