bd695c250e0b444d316c24033a73f1ff6ccec0b4
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-11 11:16:14 +0200

    Implement recentlyUsedProject

--- frontend/src/hooks/useRecentlyUsedProjects.ts
+++ frontend/src/hooks/useRecentlyUsedProjects.ts
@@ -1,8 +1,28 @@
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { Value } from "../types/Value";
 
-export const useRecentlyUsedProjects = (): Value<string[]> => {
+export const useRecentlyUsedProjects = (): [
+  recentlyUsedProjects: string[],
+  insertRecentlyUsedProject: (id: string) => void
+] => {
   const context = useContext(AppContext);
-  return context.recentlyUsedProjects;
+
+  const insertRecentlyUsedProject = (projectId: string) => {
+    context.recentlyUsedProjects[1]((previous) => {
+      // delete id if already exist
+      const index = previous.findIndex((item) => item === projectId);
+      if (index !== -1) {
+        previous.splice(index, 1);
+      }
+
+      // add the id to the first position
+      previous = [projectId, ...previous];
+
+      // only return the first 3 ids
+      previous = previous.slice(0, 3);
+      return previous;
+    });
+  };
+
+  return [context.recentlyUsedProjects[0], insertRecentlyUsedProject];
 };
