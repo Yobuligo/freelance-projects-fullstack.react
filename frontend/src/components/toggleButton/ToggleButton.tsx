478482ad5b2f95a018fbfabbbfa87dc26bd55869
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-14 15:05:34 +0200

    Display buttons for selecting day, week, month, year of a report

--- frontend/src/components/toggleButton/ToggleButton.tsx
+++ frontend/src/components/toggleButton/ToggleButton.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../button/Button";
 import { IToggleButtonOption } from "../toggleButtonGroup/IToggleButtonOption";
 import { IToggleButtonProps } from "./IToggleButtonProps";
 import styles from "./ToggleButton.module.scss";
@@ -6,11 +7,11 @@ export function ToggleButton<T extends IToggleButtonOption>(
   props: IToggleButtonProps<T>
 ) {
   return (
-    <button
+    <Button
       className={props.selected ? styles.selected : styles.unSelected}
       onClick={props.onSelect}
     >
       {props.item.title}
-    </button>
+    </Button>
   );
 }
--- frontend/src/features/timeTracker/report/reportSection/ReportSection.tsx
+++ frontend/src/features/timeTracker/report/reportSection/ReportSection.tsx
@@ -1,8 +1,38 @@
+import { useMemo } from "react";
+import { LabeledInput } from "../../../../components/labeledInput/LabeledInput";
+import { ToggleButtonGroup } from "../../../../components/toggleButtonGroup/ToggleButtonGroup";
+import { Toolbar } from "../../../../components/toolbar/Toolbar";
+import { texts } from "../../../../hooks/useTranslation/texts";
+import { useTranslation } from "../../../../hooks/useTranslation/useTranslation";
 import { IReportSectionProps } from "./IReportSectionProps";
 import styles from "./ReportSection.module.scss";
 
 export const ReportSection: React.FC<IReportSectionProps> = (props) => {
-  return <div className={styles.reportSection}>
-    Report Section
-  </div>;
+  const { t } = useTranslation();
+  const toggleButtonGroupItems = useMemo(
+    () => [
+      { key: 0, title: t(texts.general.day) },
+      { key: 1, title: t(texts.general.week) },
+      { key: 2, title: t(texts.general.month) },
+      { key: 3, title: t(texts.general.year) },
+    ],
+    [t]
+  );
+
+  return (
+    <div className={styles.reportSection}>
+      <div>
+        <Toolbar>
+          <ToggleButtonGroup
+            items={toggleButtonGroupItems}
+            selected={toggleButtonGroupItems[0]}
+          />
+        </Toolbar>
+        <Toolbar>
+          <LabeledInput label="from" type="date" />
+          <LabeledInput label="to" type="date" />
+        </Toolbar>
+      </div>
+    </div>
+  );
 };
