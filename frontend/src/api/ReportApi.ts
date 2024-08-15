e804c168258b1cdbe31d45c6e27aa24a553b82eb
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-15 09:30:14 +0200

    Implement ReportApi

---
+++ frontend/src/api/ReportApi.ts
@@ -0,0 +1,17 @@
+import { DateTime } from "../core/services/date/DateTime";
+import { IDateTimeSpan } from "../core/services/date/IDateTimeSpan";
+import { ITimeSheet, TimeSheetRouteMeta } from "../shared/model/ITimeSheet";
+import { Repository } from "./core/Repository";
+
+export class ReportApi extends Repository<any> {
+  constructor() {
+    super({ path: "/reports" });
+  }
+
+  findTimeSheets(dateTimeSpan: IDateTimeSpan): Promise<ITimeSheet[]> {
+    return this.get(`${this.url}/${TimeSheetRouteMeta.path}`, {
+      from: DateTime.toDate(dateTimeSpan.from),
+      to: DateTime.toDate(dateTimeSpan.to),
+    });
+  }
+}
---
+++ frontend/src/shared/model/ITimeSheet.ts
@@ -0,0 +1,12 @@
+import { Duration } from "../../core/services/date/Duration";
+import { IRouteMeta } from "../types/IRouteMeta";
+import { IProject } from "./IProject";
+
+export interface ITimeSheet {
+  duration: Duration;
+  from: Date;
+  project: IProject;
+  to: Date;
+}
+
+export const TimeSheetRouteMeta: IRouteMeta = { path: "/time-sheets" };
