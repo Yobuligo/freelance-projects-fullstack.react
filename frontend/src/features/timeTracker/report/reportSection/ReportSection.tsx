d97481c5c46a3bd391ccf5d233e5f8c8dd7ab762
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-15 09:09:14 +0200

    Add apply button

--- frontend/src/features/timeTracker/report/reportSection/ReportSection.tsx
+++ frontend/src/features/timeTracker/report/reportSection/ReportSection.tsx
@@ -1,11 +1,19 @@
+import { Button } from "../../../../components/button/Button";
 import { TimePeriodFilter } from "../../../../components/timePeriodFilter/TimePeriodFilter";
+import { texts } from "../../../../hooks/useTranslation/texts";
+import { useTranslation } from "../../../../hooks/useTranslation/useTranslation";
 import { IReportSectionProps } from "./IReportSectionProps";
 import styles from "./ReportSection.module.scss";
 
 export const ReportSection: React.FC<IReportSectionProps> = (props) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.reportSection}>
-      <TimePeriodFilter />
+      <div className={styles.timePeriodFilter}>
+        <TimePeriodFilter />
+        <Button className={styles.button}>{t(texts.general.apply)}</Button>
+      </div>
     </div>
   );
 };
