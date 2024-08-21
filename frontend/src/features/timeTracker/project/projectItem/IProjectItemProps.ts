252f71beeeb2567e4dae39caa9a869cd365cdba1
acoria <tewes.verena@gmail.com>
2024-08-21 10:25:50 +0200

    Tidy ProjectItem

--- frontend/src/features/timeTracker/project/projectItem/IProjectItemProps.ts
+++ frontend/src/features/timeTracker/project/projectItem/IProjectItemProps.ts
@@ -1,9 +1,7 @@
 import { IProject } from "../../../../shared/model/IProject";
 
 export interface IProjectItemProps {
-  onChange?: (project: IProject) => void;
   onClick?: (project: IProject) => void;
-  onDelete?: (project: IProject) => void;
   onStart?: (project: IProject) => void;
   onStop?: (project: IProject) => void;
   project: IProject;
