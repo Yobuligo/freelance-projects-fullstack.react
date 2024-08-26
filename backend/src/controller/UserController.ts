439846b5345dde81611c569bb9e41bb9dba4f0b0
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-26 16:20:04 +0200

    Wrap endpoints by ErrorInterceptor

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -5,6 +5,7 @@ import { ICredentials } from "../shared/model/ICredentials";
 import { ISession } from "../shared/model/ISession";
 import { createError } from "../shared/utils/createError";
 import { Controller } from "./core/Controller";
+import { ErrorInterceptor } from "./core/ErrorInterceptor";
 
 export class UserController extends Controller {
   constructor() {
@@ -15,46 +16,57 @@ export class UserController extends Controller {
   }
 
   private login() {
-    this.router.post("/users/login", async (req, res) => {
-      const authentication: IAuthentication = req.body;
-      
-      const userRepo = new UserRepo();
-      const user = await userRepo.findByCredentials(authentication.credentials);
-      if (!user) {
-        return res
-          .status(404)
-          .send(createError(`Incorrect username or password.`));
-      }
+    this.router.post(
+      "/users/login",
+      ErrorInterceptor(async (req, res) => {
+        const authentication: IAuthentication = req.body;
 
-      const sessionRepo = new SessionRepo();
-      const session = await sessionRepo.createUserSession(
-        user,
-        authentication.platform
-      );
-      res.status(201).send(session);
-    });
+        const userRepo = new UserRepo();
+        const user = await userRepo.findByCredentials(
+          authentication.credentials
+        );
+        if (!user) {
+          return res
+            .status(404)
+            .send(createError(`Incorrect username or password.`));
+        }
+
+        const sessionRepo = new SessionRepo();
+        const session = await sessionRepo.createUserSession(
+          user,
+          authentication.platform
+        );
+        res.status(201).send(session);
+      })
+    );
   }
 
   private logout() {
-    this.router.post("/users/logout", async (req, res) => {
-      const session: ISession = req.body;
-      const sessionRepo = new SessionRepo();
-      const success = await sessionRepo.deleteSession(session);
-      res.status(200).send(success);
-    });
+    this.router.post(
+      "/users/logout",
+      ErrorInterceptor(async (req, res) => {
+        const session: ISession = req.body;
+        const sessionRepo = new SessionRepo();
+        const success = await sessionRepo.deleteSession(session);
+        res.status(200).send(success);
+      })
+    );
   }
 
   private register() {
-    this.router.post("/users/register", async (req, res) => {
-      const credentials: ICredentials = req.body;
+    this.router.post(
+      "/users/register",
+      ErrorInterceptor(async (req, res) => {
+        const credentials: ICredentials = req.body;
 
-      const userRepo = new UserRepo();
-      const user = await userRepo.findByUsername(credentials.username);
-      if (user) {
-        return res.status(409).send(createError(`User already exists.`));
-      }
-      await userRepo.createUser(credentials);
-      return res.status(201).send(true);
-    });
+        const userRepo = new UserRepo();
+        const user = await userRepo.findByUsername(credentials.username);
+        if (user) {
+          return res.status(409).send(createError(`User already exists.`));
+        }
+        await userRepo.createUser(credentials);
+        return res.status(201).send(true);
+      })
+    );
   }
 }
