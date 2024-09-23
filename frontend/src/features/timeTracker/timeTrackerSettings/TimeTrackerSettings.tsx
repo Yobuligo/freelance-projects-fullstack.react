962dddc45a22dea7582b7068878c0bada4f90d71
Yobuligo <hoffmannpeter82@gmail.com>
2024-09-23 10:42:32 +0200

    Add SettingsSection

--- frontend/src/features/timeTracker/timeTrackerSettings/TimeTrackerSettings.tsx
+++ frontend/src/features/timeTracker/timeTrackerSettings/TimeTrackerSettings.tsx
@@ -1,13 +1,22 @@
+import { texts } from "../../../hooks/useTranslation/texts";
+import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
 import { Settings } from "../../settings/settings/Settings";
+import { SettingsSection } from "../../settings/settingsSection/SettingsSection";
 import { ITimeTrackerSettingsProps } from "./ITimeTrackerSettingsProps";
 import styles from "./TimeTrackerSettings.module.scss";
 
 export const TimeTrackerSettings: React.FC<ITimeTrackerSettingsProps> = (
   props
 ) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.timeTrackerSettings}>
-      <Settings />
+      <Settings>
+        <SettingsSection title={t(texts.timeTrackerSettings.general)}>
+          Hello World
+        </SettingsSection>
+      </Settings>
     </div>
   );
 };
